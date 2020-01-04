c1f9888b0aad5eb4ef285602b21d88390b694485
shostak-pgs <54119632+shostak-pgs@users.noreply.github.com>
2020-01-04 19:20:03 +0800

    Add files via upload

---
+++ src/main/java/app/config/WebConfig.java
@@ -0,0 +1,26 @@
+package app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan()
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/WEB-INF/views/**").addResourceLocations("/views/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+        final InternalResourceViewResolver resolver = new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+}
